a38c8eb2c015718900dc9278729b0a7741bae4af
Gabriel-HBR <bacarado.gabriel@gmail.com>
2025-05-19 11:40:09 -0300

    feat(settings): add Settings component

---
+++ dots-boxes/src/components/Settings.tsx
@@ -0,0 +1,24 @@
+import "../styles/Settings.css";
+
+import "../utils/gamePresets";
+import "../utils/gameUtils";
+// import { Player } from "../types/PlayerType";
+// {
+//   Players
+//   setPlayers,
+//   setBoard
+// }:{
+//   Players: Player[];
+//   setPlayers: (players: Player[]) => void;
+//   setBoard: (board: number[][]) => void;
+// }
+export default function Settings() {
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="mx-4 settings card">TESTE</div>
+      </div>
+    </>
+  );
+}
